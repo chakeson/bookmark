a87d49e95c99a12b8b556d5ebcd8a4d5156ce5bf
Carl-Henrik Åkeson <carlhenrik.akeson@gmail.com>
2021-10-05 03:54:48 +0200

    Build bookmark page, reconsidered how to build it and went with a tree structure

--- src/pages/bookmarks/bookmarks.js
+++ src/pages/bookmarks/bookmarks.js
@@ -1,18 +1,65 @@
-import react from 'react';
+import react, { useState } from 'react';
 import bookmarksData from "../../data/data"
 import { Container } from 'react-bootstrap';
-const topCategory = [...new Set(bookmarksData.map((item)=> item.category))];
+import { AiOutlineFolderAdd , AiOutlineFolderOpen} from "react-icons/ai";
+import link from "../../data/link.png"
+
+
+const Node = ({id,title,url,img,children}) => {
+    const [nodeshow, setNodeShow] = useState(false);
+
+    const handleClick = () => {
+        setNodeShow(!nodeshow);
+    }
+
+
+    return (
+        <div className="">
+            {url==="" ? (
+                <div className="d-inline-flex flex-row" style={{alignItems:"baseline"}} onClick={()=>handleClick()}>
+                    {(img!=="folder") && (children==="") && <img src={img} alt="icon" className="iconOwn" onError={(e)=>{e.target.onerror = null; e.target.src=link}}/>}
+                    {(img==="folder") && (nodeshow?<AiOutlineFolderOpen style={{fontSize:"1.3rem"}} /> : <AiOutlineFolderAdd style={{fontSize:"1.3rem"}} />)}
+                    <h4>{title}</h4>
+                </div>
+                )
+            : 
+            (
+                <a href={url} target="_blank" rel="noreferrer noopener" className="d-inline-flex flex-row linkOwn" style={{alignItems:"baseline"}} >
+                    {(children==="") && (img!=="folder") && <img src={img} alt="icon" className="iconOwn" onError={(e)=>{e.target.onerror = null; e.target.src=link}}/>}
+                    {(img==="folder") && (nodeshow?<AiOutlineFolderOpen style={{fontSize:"1.3rem"}} /> : <AiOutlineFolderAdd style={{fontSize:"1.3rem"}} />)}
+                    <h4>{title}</h4>
+                </a>
+            )
+            }
+
+            <div className="subfolder">
+                {nodeshow && (children!=="") && children.map((subnode) => (
+                    <Node {...subnode} key={subnode.id}/>
+                ))}
+            </div>
+        </div>
+    )
+
+}
 
 
 function Bookmarks() {
 
     return (
         <Container>
-            <div>
-                
-            </div>
+            {bookmarksData.map((firstLayerNodes,index) => (
+                <Node {...firstLayerNodes} key={index}/>
+            ))}
+            
         </Container>
     )
 }
 
 export default Bookmarks;
+
+
+
+/*
+    {nodeshow ? (<h4 className="fontWeightOwn">{title}</h4>) : (<h4 className="fontWeightOwnNormal">{title}</h4>) }
+    <h4 style={{fontWeight: nodeshow ? "bold":"normal"}}>{title}</h4>
+*/
