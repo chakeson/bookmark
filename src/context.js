0db7e4246faa16ef85c0d6da5df55bb6a1f915c7
chakeson <80765479+chakeson@users.noreply.github.com>
2021-10-19 04:05:23 +0200

    API work

--- src/context.js
+++ src/context.js
@@ -5,10 +5,12 @@ const AppContext = React.createContext()
 const AppProvider = ({ children }) => {
     const [succesfulFetchIP, setSuccesfulFetchIP] = useState(false);
     const [succesfulFetchSun, setSuccesfulFetchSun] = useState(false);
+    const [succesfulFetchWeather, setSuccesfulFetchWeather] = useState(false);
     const [fetchDataIP, setfetchDataIP] = useState("");
     const [fetchDataSun, setfetchDataSun] = useState("");
+    const [fetchDataWeather, setFetchDataWeather] = useState("");
     return (
-        <AppContext.Provider value={{succesfulFetchIP,setSuccesfulFetchIP, succesfulFetchSun,setSuccesfulFetchSun, fetchDataIP,setfetchDataIP, fetchDataSun,setfetchDataSun}}>
+        <AppContext.Provider value={{succesfulFetchIP,setSuccesfulFetchIP, succesfulFetchSun,setSuccesfulFetchSun, fetchDataIP,setfetchDataIP, fetchDataSun,setfetchDataSun, succesfulFetchWeather,setSuccesfulFetchWeather,fetchDataWeather, setFetchDataWeather}}>
             {children}
         </AppContext.Provider>
     )
--- src/pages/dashboard/components/yrweather.js
+++ src/pages/dashboard/components/yrweather.js
@@ -1,44 +1,64 @@
-import react, { useCallback, useEffect, useState } from 'react';
+import react, { useCallback, useEffect } from 'react';
 import { useGlobalContext } from '../../../context';
+import moment from 'moment'
+//import {} from "react-icons/wi";
 
-
-const urlWeather = "https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=60&lon=11" //"https://api.met.no/weatherapi/locationforecast/2.0/compact?"
+const urlWeather = "https://api.met.no/weatherapi/locationforecast/2.0/compact?"
 
 function YRWeather() {
-    const {succesfulFetchIP,fetchDataIP,fetchDataSun,setfetchDataSun,succesfulFetchSun,setSuccesfulFetchSun} = useGlobalContext();
+    const {succesfulFetchIP,fetchDataIP ,succesfulFetchWeather,setSuccesfulFetchWeather,fetchDataWeather, setFetchDataWeather} = useGlobalContext();
+
+    const findBreakPoint = (data) => {
+        let i = 0;
+        let breakPointWeather = 53;
+        let timeDifference = 0;
+        try {
+            while (i < data.length) {
+                let a = moment(data[i].time)
+                let b = moment(data[i+1].time)
+                timeDifference = b.diff(a,"hours")
+                console.log(timeDifference)
+                if (timeDifference===6) {
+                    breakPointWeather = i;
+                    i=data.length;
+                }
+                i++
+            }
+        } catch (error) {
+            console.log(error)
+        }
+        return breakPointWeather
+    }
 
     const getWeatherData = useCallback( async () => {
         try {
-            //const sitename = "bookmark https://github.com/chakeson/bookmark"
-            let h = new Headers({
-                    'Content-Type': 'application/json',
-                    'User-Agent': "bookmark https://github.com/chakeson/bookmark",
-                })
-            
-            const requestOptions = {
-                method: "get",
-                mode: "no-cors",
-                headers: h,
-                Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-                redirect: 'follow',
-            }
             //API call
-            const url = urlWeather;//`${urlWeather}lat=${fetchDataIP.lat}&lon=${fetchDataIP.lon}, 10)}`;
-            const response = await fetch( url , requestOptions );
-            console.log(response);
+            const url = `${urlWeather}lat=${fetchDataIP.lat.toFixed(4)}&lon=${fetchDataIP.lon.toFixed(4)}`;
+            //console.log(url)
+            const response = await fetch( url );
+            if (!response.ok || (response.status!==200)) {
+                throw new Error("HTTP status" + response.status + " "+ response.statusText)
+            }
+            //console.log(response);
             const data = await response.json();
             console.log(data);
-            //setfetchDataSun(data.properties.timeseries)
-            //setSuccesfulFetchSun(true);
+            
+            // Process the data and extract when it goes from hourly to long term (6h gap)
+            const breakPointWeather = findBreakPoint(data.properties.timeseries)
+
+            //console.log(data.properties.timeseries)
+            setFetchDataWeather([data.properties.timeseries,"dgsdg"])
+            //console.log(fetchDataWeather)
+            setSuccesfulFetchWeather(true);
         } catch (error) {
             if (error.message === "Failed to fetch") {
-                //setSuccesfulFetchSun(false);
-                //setfetchDataSun("Adblock");
+                setSuccesfulFetchWeather(false);
+                setFetchDataWeather("Adblock");
             }
             console.log(error)
-            //setSuccesfulFetchSun(false);
+            setSuccesfulFetchWeather(false);
         }
-    },[fetchDataIP,setSuccesfulFetchSun,setfetchDataSun])
+    },[fetchDataIP,setFetchDataWeather,setSuccesfulFetchWeather])
 
     useEffect(()=>{
         if ( succesfulFetchIP === true ){
@@ -49,8 +69,8 @@ function YRWeather() {
 
     return (
         <div className="card">
-            <div>Sunrise: {succesfulFetchSun ? fetchDataSun.sunrise.slice(11,16) : `loading: ${fetchDataSun}` }</div>
-            <div>Sunset: {succesfulFetchSun ? fetchDataSun.sunset.slice(11,16) : `Loading: ${fetchDataSun}`} </div>
+            {/*<div>Sunrise: {succesfulFetchWeather ? fetchDataWeather : `loading: ${fetchDataWeather}` }</div>
+            <div>Sunset: {succesfulFetchWeather ? fetchDataWeather : `Loading: ${fetchDataWeather}`} </div>*/}
         </div>
         
     )
